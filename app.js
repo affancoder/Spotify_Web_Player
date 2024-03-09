d9bc35d619ff45478d9e0c6e2b9d00d20317e460
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2024-03-09 16:12:09 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+console.log("Welcome to spotify - Web Player");
+
 document.addEventListener("DOMContentLoaded", function () {
   const menuToggle = document.getElementById("menu-toggle");
   const navbarMenu = document.querySelector(".sidebar");
@@ -5,4 +7,17 @@ document.addEventListener("DOMContentLoaded", function () {
   menuToggle.addEventListener("click", function () {
     navbarMenu.classList.toggle("active");
   });
+});
+
+let card = document.getElementById("card");
+let btnplay = document.querySelectorAll(".btnplay");
+let masterplay = document.getElementById("masterplay");
+
+card.addEventListener("mousemove", function () {
+  btnplay.style.display = "flex";
+  card.style.transition = "1s";
+});
+
+masterplay.addEventListener("click", function (e) {
+  masterplay.classList.add("fa-solid fa-pause");
 });
